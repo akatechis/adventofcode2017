9518511283f4216c17b1bb828a640f7711a4ef59
Alexandros Katechis <akatechis@gmail.com>
2018-01-04 13:47:03 -0500

    Day 17 solution.

--- src/seventeen.rs
+++ src/seventeen.rs
@@ -1,29 +1,40 @@
 
-fn value_after_n_inserts(buffer: &mut Vec<usize>, step: usize, target: usize) -> usize {
+fn value_after_n_inserts(buffer: &mut Vec<usize>, step: usize, inserts: usize, value_after: usize) -> usize {
   let mut ptr = 0;
-  let mut value = 1;
-  while value <= target {
-    // step forward
+  for value in 1..inserts + 1 {
     ptr = (ptr + step) % buffer.len();
-
-    // insert
     buffer.insert(ptr + 1, value);
-
-    // go to [value]
-    ptr = value;
-
-    println!("ptr = {}", ptr);
-    value += 1;
+    ptr += 1;
   }
 
-  buffer[ptr + 1]
+  let pos = buffer.iter().position(|&v|v==value_after).unwrap();
+  buffer[pos + 1]
+}
+
+fn spinlock(step: usize) -> usize {
+  let mut current = 0;
+  let mut one_slot = 0;
+  for v in 1..50_000_000+1 {
+    current = ((current + step) % v) + 1;
+    if current == 1 {
+      one_slot = v;
+    }
+  }
+  one_slot
 }
 
 pub fn main () {
-  let step = 377;
-  let target = 2017;
-  let mut buf = vec![0];
-  let answer = value_after_n_inserts(&mut buf, step, target);
+  {
+    let step = 377;
+    let inserts = 2017;
+    let mut buf = vec![0];
+    let answer = value_after_n_inserts(&mut buf, step, inserts, inserts);
+    println!("Position after 2017: {}", answer);
+  }
+  {
+    let answer = spinlock(377);
+    println!("Position after 0: {}", answer);
+  }
 }
 
 #[cfg(test)]
@@ -32,7 +43,8 @@ mod tests {
 
   #[test]
   fn value_after_n_inserts_works() {
-    let mut buf = vec![0];
-    assert_eq!(1, value_after_n_inserts(&mut buf, 3, 3));
+    assert_eq!(1, value_after_n_inserts(&mut vec![0], 3, 3, 3));
+    assert_eq!(638, value_after_n_inserts(&mut vec![0], 3, 2017, 2017));
+    assert_eq!(151, value_after_n_inserts(&mut vec![0], 3, 2017, 1134));
   }
 }
